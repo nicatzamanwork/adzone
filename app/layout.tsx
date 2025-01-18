96fe677194cc8ab4ef7deb48189acc3074692419
Nicat <webdevelopernicat@gmail.com>
2025-01-18 12:55:16 +0400

    russian

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,50 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n";
 import "./globals.css";
+import { usePathname } from "next/navigation"; // For tracking route changes
+import Script from "next/script";
+import { GA_TRACKING_ID, pageview } from "../utils/googleAnalytics";
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // Track page views on route changes
+    pageview(pathname);
+  }, [pathname]);
+
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body>
-        <I18nextProvider i18n={i18n}>
-          {children}
-        </I18nextProvider>
+        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
       </body>
     </html>
   );
